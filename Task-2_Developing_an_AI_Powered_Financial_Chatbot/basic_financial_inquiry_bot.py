e5209168310583f31f17935b031d0f4a089ff575
barisparlakk <barisparlak36@gmail.com>
2024-12-25 14:57:16 +0300

    Add and update chatbot

---
+++ Task-2_Developing_an_AI_Powered_Financial_Chatbot/basic_financial_inquiry_bot.py
@@ -0,0 +1,11 @@
+def simple_chatbot(user_query):
+    if user_query.lower() == "what is the total revenue?":
+        return "The total revenue is [amount]."
+    elif user_query.lower() == "how was net income has changed over the last year?":
+        return "The net income has increased/decreased by [amount] over the last year."
+    elif user_query.lower() == "what is the current cash flow?":
+        return "The current cash flow is [amount]."
+
+user_query = input("Please enter your query: ")
+response = simple_chatbot(user_query)
+print(response)
